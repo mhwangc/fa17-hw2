aa41b66ff53123f21d799221723b8ae2adbda88a
mhwangc <mhwangc@berkeley.edu>
2017-09-27 02:00:09 -0700

    finish hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,12 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  attr_accessor :baz
+
   def initialize(baz)
+  	@baz = baz
+  end
+
+  def bar(regular, hash)
+  	regular.to_s + @baz.to_s+ hash[:sat].to_s
   end
 end
